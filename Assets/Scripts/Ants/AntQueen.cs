c256379fd9df8f40eb5db8467d2dde89eaab9700
Rafagcl <rafagonzalezdecanales4+Multimedia@gmail.com>
2024-04-03 20:38:15 +0200

    Update metodos antqueen

--- Assets/Scripts/Ants/AntQueen.cs
+++ Assets/Scripts/Ants/AntQueen.cs
@@ -7,29 +7,138 @@ using UnityEngine;
 
 public class AntQueen : Ant
 {
-    public int vida;
-    bool alimentada;
+    public float tiempoDeVida;
+    public float tiempoVidaLimite;
+    public float tiempoIncubacion = 10f;
+    public float tiemoFabricacionJalea = 15f;
 
+    bool alimentada = false;
+    bool incubado = false;
+    bool jalea = false;
+
+    void Awake()
+    {
+        Initialize();
+    }
     public override void Initialize()
     {
-        
-    }
+        StartWaitForFood = () =>
+        {
+            Debug.Log("Esperando a ser alimentada...");
+        };
 
+        UpdateWaitForFood = () =>
+        {
+            // Verificar si la reina está alimentada
+            if (alimentada)
+            {
+                // Verificar si el tiempo de vida es menor que x
+                if (tiempoDeVida < tiempoVidaLimite)
+                {
+                    // Cambiar al estado de poner huevo
+                    StartLayEgg();
+                    return Status.Success;
+                }
+                else
+                {
+                    // Generar jalea real
+                    StartGenerateRoyalJelly();
+                    return Status.Success;
+                }
+            }
+            else
+            {
+                // No cambiar de estado
+                return Status.Running;
+            }
+        };
+
+        StartLayEgg = () =>
+        {
+            Debug.Log("Poniendo huevo...");
+            float tiempoTranscurridoIncubacion = 0f;
+            while (tiempoTranscurridoIncubacion < tiemoFabricacionJalea)
+            {
+                // Actualizar el tiempo transcurrido
+                tiempoTranscurridoIncubacion += Time.deltaTime;
+                jalea = true;
+
+                //AQUÍ SE GENERARÁ UNA LARVA
+            };
+
+            UpdateLayEgg = () =>
+            {
+                tiempoTranscurridoIncubacion += Time.deltaTime;
+
+                if (incubado && tiempoTranscurridoIncubacion >= tiemoFabricacionJalea)
+                {
+                    jalea = false;
+                    StartWaitForFood(); // Vuelve al estado de espera
+                    return Status.Success;
+                }
+                else
+                {
+                    // Verificar otras condiciones si es necesario
+                    return Status.Running;
+                }
+            };
+        };
+
+        StartGenerateRoyalJelly = () =>
+        {
+            Debug.Log("Generando Jalea Real...");
+            float tiempoTranscurridoGenerarJalea = 0f;
+            while (tiempoTranscurridoGenerarJalea < tiempoIncubacion)
+            {
+                // Actualizar el tiempo transcurrido
+                tiempoTranscurridoGenerarJalea += Time.deltaTime;
+                jalea = true;
+
+                //AQUÍ SE GENERARÁ la Jalea
+            };
+
+            UpdateGenerateRoyalJelly = () =>
+            {
+                tiempoTranscurridoGenerarJalea += Time.deltaTime;
+
+                if (jalea && tiempoTranscurridoGenerarJalea >= tiempoIncubacion)
+                {
+                    incubado = false;
+                    StartWaitForFood(); // Vuelve al estado de espera
+                    return Status.Success;
+                }
+                else
+                {
+                    // Verificar otras condiciones si es necesario
+                    return Status.Running;
+                }
+            };
+
+            StartDie = () =>
+            {
+                Die();
+            };
+        };
+        }
     public System.Action StartWaitForFood = () => {
-        Debug.Log("Esperando a ser alimentada...");
+        // Implementa la lógica para que la reina espere a ser alimentada
+        // Esto podría incluir animaciones, sonidos, etc.
+        Debug.Log("Generando Jalea Real...");
     };
     public Func<Status> UpdateWaitForFood = () => {
-        // Implementa la lógica para actualizar el estado de la reina mientras espera a ser alimentada
+        // Implementa la lógica para actualizar el estado de la reina mientras pone un huevo
         // Puede incluir la verificación de ciertas condiciones y devolver el estado apropiado
-        Debug.Log("Actualizando estado mientras espera a ser alimentada...");
+        Debug.Log("Actualizando estado mientras la reina pone un huevo...");
         return Status.Running; // Ejemplo: devolver un estado ficticio
     };
 
-    public System.Action StartLayEgg = () => {
+    public System.Action StartLayEgg = () =>
+    {
         // Implementa la lógica para que la reina espere a ser alimentada
         // Esto podría incluir animaciones, sonidos, etc.
-        Debug.Log("Poniendo huevo...");
+        Debug.Log("Generando Jalea Real...");
     };
+
     public Func<Status> UpdateLayEgg = () => {
         // Implementa la lógica para actualizar el estado de la reina mientras pone un huevo
         // Puede incluir la verificación de ciertas condiciones y devolver el estado apropiado
@@ -54,12 +163,7 @@ public class AntQueen : Ant
         // Esto podría incluir animaciones, sonidos, etc.
         Debug.Log("Muriendo...");
     };
-    public Func<Status> UpdateDie = () => {
-        // Implementa la lógica para actualizar el estado de la reina mientras pone un huevo
-        // Puede incluir la verificación de ciertas condiciones y devolver el estado apropiado
-        Debug.Log("Actualizando estado mientras la reina muere...");
-        return Status.Running; // Ejemplo: devolver un estado ficticio
-    };
+    
 
 
 
