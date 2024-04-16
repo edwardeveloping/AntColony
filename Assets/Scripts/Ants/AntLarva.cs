42efeea7ed850ccf319c013395437945fd3c961e
ErJimy <j.hereza.2020@alumnos.urjc.es>
2024-04-16 18:39:23 +0200

    Born
    Co-Authored-By: janmorata <77397315+janmorata@users.noreply.github.com>

--- Assets/Scripts/Ants/AntLarva.cs
+++ Assets/Scripts/Ants/AntLarva.cs
@@ -6,7 +6,32 @@ public class AntLarva : Ant
 {
 
     //Variables
+    public float bornTime = 10f;
+    public string type = "Gatherer";
+    [SerializeField] public Colony colony;
 
+    private void Update()
+    {
+        bornTime -= Time.deltaTime;
+
+        if ( bornTime <= 0)
+        {
+            Born();
+        }
+    }
+
+    public void Born()
+    {
+        if (type == "Gatherer")
+        {
+            colony.Initialize("Gatherer");
+        }
+
+        if (type == "Worker")
+        {
+            colony.Initialize("Worker");
+        }
+    }
     public override void Initialize()
     {
         
