50d6cb73b4e293847860165032d546d9a21ea962
Rafagcl <rafagonzalezdecanales4+Multimedia@gmail.com>
2024-06-17 19:48:49 +0200

    Control de variables

--- Assets/Scripts/Colony.cs
+++ Assets/Scripts/Colony.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Colony : MonoBehaviour
 {
@@ -14,15 +16,14 @@ public class Colony : MonoBehaviour
 
     int shells;
 
-    //
     public Room storageRoom;
 
     //variables para controlar desde el menu de Unity compilando el programa
-    public int initial_Gatherers;
-    public int initial_Workers;
-    public int initial_Soldiers;
-    public int initial_Predators;
-    public int initial_Escarabajos;
+    private int initial_Gatherers;
+    private int initial_Workers;
+    private int initial_Soldiers;
+    private int initial_Predators;
+    private int initial_Escarabajos;
     public int population;
     public int totalGatherers;
     public int totalWorkers;
@@ -33,45 +34,106 @@ public class Colony : MonoBehaviour
     public int totalPredators;
     public int storageResources;
 
-
     //auxiliar
     private int auxiliarSoldierCount;
+
+    // UI Elements
+    public Slider sliderGatherers;
+    public Slider sliderWorkers;
+    public Slider sliderSoldiers;
+    public Slider sliderPredators;
+    public Slider sliderEscarabajos;
+    public Button buttonStartSimulation;
+
+    // Text elements to display slider values
+    public TMP_Text textGatherersValue;
+    public TMP_Text textWorkersValue;
+    public TMP_Text textSoldiersValue;
+    public TMP_Text textPredatorsValue;
+    public TMP_Text textEscarabajosValue;
+
+    // Referencias a otros scripts
+    public GameObject gameManager;
+    public MonoBehaviour mapScript;
+    public MonoBehaviour predatorManagerScript;
 
     private void Start()
     {
         auxiliarSoldierCount = 0;
+        buttonStartSimulation.onClick.AddListener(IniciarSimulacion);
+        this.enabled = false; // Desactivar el script al inicio
+        gameManager.SetActive(false); // Desactivar el GameManager al inicio
+        mapScript.enabled = false; // Desactivar el script Map al inicio
+        predatorManagerScript.enabled = false;
+
+        // Add listeners to update text values when sliders change
+        sliderGatherers.onValueChanged.AddListener(delegate { UpdateSliderValue(sliderGatherers, textGatherersValue); });
+        sliderWorkers.onValueChanged.AddListener(delegate { UpdateSliderValue(sliderWorkers, textWorkersValue); });
+        sliderSoldiers.onValueChanged.AddListener(delegate { UpdateSliderValue(sliderSoldiers, textSoldiersValue); });
+        sliderPredators.onValueChanged.AddListener(delegate { UpdateSliderValue(sliderPredators, textPredatorsValue); });
+        sliderEscarabajos.onValueChanged.AddListener(delegate { UpdateSliderValue(sliderEscarabajos, textEscarabajosValue); });
+
+        // Initialize text values
+        UpdateSliderValue(sliderGatherers, textGatherersValue);
+        UpdateSliderValue(sliderWorkers, textWorkersValue);
+        UpdateSliderValue(sliderSoldiers, textSoldiersValue);
+        UpdateSliderValue(sliderPredators, textPredatorsValue);
+        UpdateSliderValue(sliderEscarabajos, textEscarabajosValue);
     }
-    public void Initialize(string s)
+
+    private void IniciarSimulacion()
     {
-        if (s == "Init") //INICIALIZACION
+        initial_Gatherers = (int)sliderGatherers.value;
+        initial_Workers = (int)sliderWorkers.value;
+        initial_Soldiers = (int)sliderSoldiers.value;
+        initial_Predators = (int)sliderPredators.value;
+        initial_Escarabajos = (int)sliderEscarabajos.value;
+
+        // Inicialización de la simulación
+        for (int i = 0; i < initial_Gatherers; i++)
         {
-            for (int i = 0; i < initial_Gatherers; i++)
-            {
-                antManager.GenerateAnt(0, -5, AntManager.Role.Gatherer);
-            }
-
-            for (int i = 0; i < initial_Workers; i++)
-            {
-                antManager.GenerateAnt(-17, -6, AntManager.Role.Worker);
-            }
-
-            for (int i = 0;i < initial_Soldiers; i++)
-            {
-                antManager.GenerateAnt(-10, -10, AntManager.Role.Soldier);
-            }
-
-            antManager.GenerateAnt(20, -7, AntManager.Role.Queen);
-
-            //depredadores
-            predatorManager.initialNumPredators = initial_Predators;
-            StartCoroutine(predatorManager.GeneratePredatorsOverTime(initial_Predators));
-
-            //Escarabajos
-            escarabajoManager.initialNumBeetles = initial_Escarabajos;
-            StartCoroutine(escarabajoManager.GenerateBeetleOverTime(initial_Escarabajos));
+            antManager.GenerateAnt(0, -5, AntManager.Role.Gatherer);
         }
-    }
 
+        for (int i = 0; i < initial_Workers; i++)
+        {
+            antManager.GenerateAnt(-17, -6, AntManager.Role.Worker);
+        }
+
+        for (int i = 0; i < initial_Soldiers; i++)
+        {
+            antManager.GenerateAnt(-10, -10, AntManager.Role.Soldier);
+        }
+
+        antManager.GenerateAnt(20, -7, AntManager.Role.Queen);
+
+        //depredadores
+        predatorManager.initialNumPredators = initial_Predators;
+        StartCoroutine(predatorManager.GeneratePredatorsOverTime(initial_Predators));
+
+        //Escarabajos
+        escarabajoManager.initialNumBeetles = initial_Escarabajos;
+        StartCoroutine(escarabajoManager.GenerateBeetleOverTime(initial_Escarabajos));
+
+        // Ocultar el canvas de configuración
+        GameObject configCanvas = GameObject.Find("ConfigCanvas");
+        if (configCanvas != null)
+        {
+            configCanvas.SetActive(false);
+        }
+
+        // Activar el script
+        this.enabled = true;
+
+        // Activar el GameManager
+        gameManager.SetActive(true);
+
+        // Activar el script Map
+        mapScript.enabled = true;
+
+        // Activar el script predatorManaher
+        predatorManagerScript.enabled = true;
+    }
 
     private void Update()
     {
@@ -90,12 +152,8 @@ public class Colony : MonoBehaviour
         int resources = storageRoom.GetComponent<Room>().count;
         int gathererLarvaCount = CountPendingLarvas("Gatherer");
         int workerLarvaCount = CountPendingLarvas("Worker");
-
-        
-
-        
-
     }
+
     private void ManageColony()
     {
         int gathererNumber = antManager.antGathererObjectList.Count;
@@ -104,28 +162,15 @@ public class Colony : MonoBehaviour
         int resources = storageRoom.GetComponent<Room>().count;
         int predatorNumber = predatorManager.predators.Count;
 
-        //Comprobar continuamente las listas de las hormigas para asignarle el tag correcto de cada larva
-        //Jerarquia => Gatherer > worker
-
-        //Ahora tambien necesitamos que la colonia tenga nocion de la cantidad de larvas que tienen ya dicho tag creado, ya que pdriamos crear 5 gatherer
-        //antes de que nazca la primera larva
-
         int gathererLarvaCount = CountPendingLarvas("Gatherer");
         int workerLarvaCount = CountPendingLarvas("Worker");
         int soldierLarvaCount = CountPendingLarvas("Soldier");
 
-
-        //AUXILIAR PARA EL CONTROL DESDE EL MENU DEL JUEGO
         AuxiliarControl(gathererNumber, workerNumber, soldierNumber, gathererLarvaCount, workerLarvaCount, resources, predatorNumber, soldierLarvaCount);
 
-        //Llamamos a nuestro metodo
         string type = DecideAntType(gathererNumber, workerNumber, gathererLarvaCount, workerLarvaCount);
 
-        //Llamamos a nuestra funcion
         AssignLarvaType(type);
-
-        
-
     }
 
     private void AuxiliarControl(int gathererNumber, int workerNumber, int soldierNumber, int gathererLarvaCount, int workerLarvaCount, int resources, int predatorCount, int soldierLarvaCount)
@@ -143,36 +188,26 @@ public class Colony : MonoBehaviour
 
     private string DecideAntType(int gathererNumber, int workerNumber, int gathererLarvaCount, int workerLarvaCount)
     {
-        //Si no existen Gatherers en la lista ni tampoco en las larvas pendientes por nacer, crearemos una Gatherer
         if (gathererNumber == 0 && gathererLarvaCount == 0)
         {
             return "Gatherer";
         }
-
-        //Si existen mas gatherers que workers pero ninguna en larvas, haremos worker
         else if (gathererNumber > workerNumber && workerLarvaCount == 0)
         {
             return "Worker";
         }
-
-        //Si hay muchas gatherer y muchas workers, soldier
         else if (gathererNumber >= 3 && workerNumber >= 3)
         {
             return "Soldier";
         }
-
-        //Si ninguna de las anteriores se cumple, entonces iremos a descarte, dando prioridad a las gatherer
-
         else if (gathererLarvaCount == 0)
         {
             return "Gatherer";
         }
-
         else if (workerLarvaCount == 0)
         {
             return "Worker";
         }
-
         else
         {
             return gathererNumber <= workerNumber ? "Gatherer" : "Worker";
@@ -181,14 +216,12 @@ public class Colony : MonoBehaviour
 
     private void AssignLarvaType(string antType)
     {
-        //recorremos la lista de las larvas
         foreach (var larva in antManager.antLarvaList)
         {
-            //si la larva aun no tiene un valor en su tipo
             if (string.IsNullOrEmpty(larva.GetComponent<AntLarva>().type))
             {
-                larva.GetComponent<AntLarva>().type = antType; //establecemos el tipo
-                break; //IMPORTANTE ya que si no se pondria el string a todas nuestras larvas
+                larva.GetComponent<AntLarva>().type = antType;
+                break;
             }
         }
     }
@@ -196,7 +229,7 @@ public class Colony : MonoBehaviour
     private int CountPendingLarvas(string antType)
     {
         int count = 0;
-        foreach(var larva in antManager.antLarvaList)
+        foreach (var larva in antManager.antLarvaList)
         {
             if (larva.GetComponent<AntLarva>().type == antType)
             {
@@ -207,6 +240,8 @@ public class Colony : MonoBehaviour
         return count;
     }
 
-
-
+    private void UpdateSliderValue(Slider slider, TMP_Text text)
+    {
+        text.text = slider.value.ToString();
+    }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         map.Initilize();
-        colony.Initialize("Init");
+        //colony.Initialize("Init");
         //predatorManager.Initialize();
     }
 }
